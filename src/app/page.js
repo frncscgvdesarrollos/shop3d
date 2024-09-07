71d88f58a357daf2a7d90be17ed052d26672437b
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2024-09-07 20:58:32 +0000

    primer commit

--- src/app/page.js
+++ src/app/page.js
@@ -92,68 +92,88 @@ export default function Home() {
             </section>
           </div>
         </section>
-        <section className="bg-teal-800 py-12 px-4 sm:px-6 lg:px-8 clienteFeliz">
-  <div className="max-w-3xl lg:ml-auto lg:mr-[8vw] text-center">
-    <h2 className="text-3xl sm:text-4xl font-extrabold text-orange-300 mb-8">
-      ¿Cómo Funciona SHOP3D?
+        <section className="flex flex-col lg:flex-row bg-teal-900 py-12 px-4 sm:px-6 lg:px-8 h-[90vh] quienesSomos">
+  <div className="lg:w-1/3 lg:mr-auto lg:ml-[20vw] lg:pr-20 mx-auto flex flex-col items-start justify-center">
+    <h2 className="text-3xl sm:text-4xl font-extrabold text-orange-300 mb-6 text-center lg:text-left">
+      ¿Quiénes Somos?
     </h2>
-    <p className="text-lg text-teal-200 mb-8">
-      En SHOP3D, nuestro proceso es simple y eficiente para que puedas disfrutar de nuestros productos y servicios sin complicaciones. A continuación te mostramos cómo funciona todo:
+    <p className="text-lg text-teal-200 mb-6 text-center lg:text-left leading-relaxed">
+      En SHOP3D, nuestra misión es revolucionar el mundo de la impresión 3D ofreciendo soluciones innovadoras y accesibles. Nos especializamos en transformar tus ideas en productos físicos con una calidad excepcional y a precios competitivos.
     </p>
-    <div className="space-y-8">
-      <div className="flex items-center justify-around lg:justify-start space-x-4">
-        <div className="w-16 h-16 flex items-center justify-center bg-teal-700 text-white rounded-full shadow-lg text-2xl">
-          $
-        </div>
-        <div className="text-left">
-          <h3 className="text-xl font-semibold text-orange-300 mb-2">Compra</h3>
-          <p className="text-teal-200">
-            Realiza tu compra en SHOP3D seleccionando entre nuestros productos o servicios. Asegúrate de revisar los detalles antes de confirmar tu pedido.
-          </p>
-        </div>
-      </div>
-      <div className="flex items-center justify-around lg:justify-start space-x-4">
-        <div className="w-16 h-16 flex items-center justify-center bg-teal-700 text-white rounded-full shadow-lg">
-          <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3h-2v7H4v2h7v7h2v-7h7v-2h-7z"></path>
-          </svg>
-        </div>
-        <div className="text-left">
-          <h3 className="text-xl font-semibold text-orange-300 mb-2">Impresión</h3>
-          <p className="text-teal-200">
-            Una vez realizada la compra, nuestra impresora imprimirá el producto según tus especificaciones. Utilizamos tecnología de punta para garantizar calidad.
-          </p>
-        </div>
-      </div>
-      <div className="flex items-center justify-around lg:justify-start space-x-4">
-        <div className="w-12 h-16 flex items-center justify-center bg-teal-700 text-white rounded-full shadow-lg">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7l9 5 9-5v11l-9 5-9-5V7z"></path>
-          </svg>
-        </div>
-        <div className="text-left">
-          <h3 className="text-xl font-semibold text-orange-300 mb-2">Empaque</h3>
-          <p className="text-teal-200">
-            Después de la impresión, empaquetamos cuidadosamente tu producto para asegurar que llegue en perfectas condiciones.
-          </p>
-        </div>
-      </div>
-      <div className="flex items-center justify-around lg:justify-start space-x-4">
-        <div className="w-16 h-16 flex items-center justify-center bg-teal-700 text-white rounded-full shadow-lg">
-          <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 11h1l1 3h14l1-3h1v-2H3v2zm0 0V7a1 1 0 011-1h16a1 1 0 011 1v4"></path>
-          </svg>
-        </div>
-        <div className="text-left">
-          <h3 className="text-xl font-semibold text-orange-300 mb-2">Envío</h3>
-          <p className="text-teal-200">
-            Utilizamos Andriani para el envío. Una vez enviado, te proporcionaremos un identificador de seguimiento para que puedas rastrear tu pedido en todo momento.
-          </p>
-        </div>
-      </div>
-    </div>
+    <p className="text-lg text-teal-200 mb-6 text-center lg:text-left leading-relaxed">
+      Conectamos a nuestros clientes con una red diversa de impresoras, tanto propias como de terceros, para garantizar que cada proyecto se lleve a cabo con precisión y cuidado. Desde artículos prácticos hasta piezas decorativas, nuestro objetivo es hacer realidad tus proyectos con la mejor tecnología disponible.
+    </p>
+    <p className="text-lg text-teal-200 mb-6 text-center lg:text-left leading-relaxed">
+      Nos enorgullece ser un puente entre la tecnología avanzada y las necesidades de nuestros clientes, facilitando una experiencia de impresión 3D sin igual. Descubre cómo SHOP3D puede ayudarte a llevar tus ideas al siguiente nivel.
+    </p>
+    <a href="#contact" className="text-orange-300 font-semibold hover:underline text-lg">
+      ¡Explora nuestras soluciones y únete a la revolución de la impresión 3D hoy mismo!
+    </a>
   </div>
 </section>
+
+        <section className="bg-teal-800 py-12 px-4 sm:px-6 lg:px-8 clienteFeliz">
+          <div className="max-w-3xl lg:ml-auto lg:mr-[8vw] text-center">
+            <h2 className="text-3xl sm:text-4xl font-extrabold text-orange-300 mb-8">
+              ¿Cómo Funciona SHOP3D?
+            </h2>
+            <p className="text-lg text-teal-200 mb-8">
+              En SHOP3D, nuestro proceso es simple y eficiente para que puedas disfrutar de nuestros productos y servicios sin complicaciones. A continuación te mostramos cómo funciona todo:
+            </p>
+            <div className="space-y-8">
+              <div className="flex items-center justify-around lg:justify-start space-x-4">
+                <div className="w-16 h-16 flex items-center justify-center bg-teal-700 text-white rounded-full shadow-lg text-2xl">
+                  $
+                </div>
+                <div className="text-left">
+                  <h3 className="text-xl font-semibold text-orange-300 mb-2">Compra</h3>
+                  <p className="text-teal-200">
+                    Realiza tu compra en SHOP3D seleccionando entre nuestros productos o servicios. Asegúrate de revisar los detalles antes de confirmar tu pedido.
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center justify-around lg:justify-start space-x-4">
+                <div className="w-16 h-16 flex items-center justify-center bg-teal-700 text-white rounded-full shadow-lg">
+                  <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3h-2v7H4v2h7v7h2v-7h7v-2h-7z"></path>
+                  </svg>
+                </div>
+                <div className="text-left">
+                  <h3 className="text-xl font-semibold text-orange-300 mb-2">Impresión</h3>
+                  <p className="text-teal-200">
+                    Una vez realizada la compra, nuestra impresora imprimirá el producto según tus especificaciones. Utilizamos tecnología de punta para garantizar calidad.
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center justify-around lg:justify-start space-x-4">
+                <div className="w-12 h-16 flex items-center justify-center bg-teal-700 text-white rounded-full shadow-lg">
+                  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7l9 5 9-5v11l-9 5-9-5V7z"></path>
+                  </svg>
+                </div>
+                <div className="text-left">
+                  <h3 className="text-xl font-semibold text-orange-300 mb-2">Empaque</h3>
+                  <p className="text-teal-200">
+                    Después de la impresión, empaquetamos cuidadosamente tu producto para asegurar que llegue en perfectas condiciones.
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-center justify-around lg:justify-start space-x-4">
+                <div className="w-16 h-16 flex items-center justify-center bg-teal-700 text-white rounded-full shadow-lg">
+                  <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 11h1l1 3h14l1-3h1v-2H3v2zm0 0V7a1 1 0 011-1h16a1 1 0 011 1v4"></path>
+                  </svg>
+                </div>
+                <div className="text-left">
+                  <h3 className="text-xl font-semibold text-orange-300 mb-2">Envío</h3>
+                  <p className="text-teal-200">
+                    Utilizamos Andriani para el envío. Una vez enviado, te proporcionaremos un identificador de seguimiento para que puedas rastrear tu pedido en todo momento.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
 
 
         <section className='relative h-[90vh] w-full flex flex-col lg:flex-row justify-center items-center bg-teal-50 p-6'>
