e54d629a9e1abd0825b276770c496e9df9e9fc07
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2024-09-07 20:47:11 +0000

    primer commit

--- src/app/shop/venta/failure/page.jsx
+++ src/app/shop/venta/failure/page.jsx
@@ -2,7 +2,7 @@ export default function Failure(){
     return(
         <>
         <span>error en el pago la venta se a cancelado</span>
-        <button onClick={()=> window.location.href = '/shop'}>cancelar</button>
+        {/* <button onClick={()=> window.location.href = '/shop'}>cancelar</button> */}
         </>
     )
 }
--- src/app/shop/venta/pending/page.jsx
+++ src/app/shop/venta/pending/page.jsx
@@ -2,7 +2,7 @@ export default function Pending(){
     return(
         <>
         <span> pago pendiente--- vuelva a tratar --- venta cancelada por precaucion </span>
-        <button onClick={()=> window.location.href = '/shop'}>cancelar</button>
+        {/* <button onClick={()=> window.location.href = '/shop'}>cancelar</button> */}
         </>
     )
 }
