8eeaee573e0b062e57cf515ced7bcae43d9a21ed
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2024-09-07 20:19:34 +0000

    primer commit

--- src/app/components/ListProductsCliente.jsx
+++ src/app/components/ListProductsCliente.jsx
@@ -177,7 +177,9 @@ export default function ListProducts() {
                 className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 transition text-gray-600"
               />
               <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
-                🔍
+               
+                 🔍
+               
               </span>
             </div>
             <select 
