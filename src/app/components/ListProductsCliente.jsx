647600c5f38bc773e769eed6d437fab9e791b512
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2024-09-07 20:26:35 +0000

    primer commit

--- src/app/components/ListProductsCliente.jsx
+++ src/app/components/ListProductsCliente.jsx
@@ -178,7 +178,7 @@ export default function ListProducts() {
               />
               <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
                
-                 🔍
+                 
                
               </span>
             </div>
