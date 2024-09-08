553cb1d3a57ad0b39daec2fcb8a504b30d166240
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2024-09-08 16:05:13 +0000

    second commit

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -59,12 +59,12 @@ export default function Header() {
   return (
     <header className="relative py-2 h-[3rem] text-white flex flex-col lg:flex-row items-center py-8 px-4 lg:px-16 z-[999]">
       <Cubo />
-      <div onClick={() => setMisDatos(!misdatos)} className="absolute top-2 right-2 text-xl">
+      <div onClick={() => setMisDatos(!misdatos)} className="absolute top-2 right-2 text-xl flex items-end justify-end ">
         <div className="flex items-center gap-2 rounded-lg transition-transform transform hover:scale-105 cursor-pointer">
           {user ? (
             <>
-              <span className="text-lg font-medium text-teal-800">Hola, {user.displayName || "Usuario"}</span>
-              <button onClick={handleLogout} className="bg-teal-500 hover:bg-teal-600 text-teal-800 font-semibold py-2 px-4 rounded ml-4">Cerrar Sesión</button>
+              <span className="text-sm lg:text-lg font-medium text-teal-800">Hola, {user.displayName || "Usuario"}</span>
+              <button onClick={handleLogout} className="bg-teal-500 hover:bg-teal-600 text-teal-800 font-semibold py-2 px-2 rounded ml-4 text-sm lg:text-lg">Cerrar Sesión</button>
             </>
           ) : (
             <div>
@@ -79,9 +79,14 @@ export default function Header() {
 
       {misdatos && user && clientData && (
         <div className="absolute top-0 right-0 bg-teal-200 text-teal-900 p-6 rounded-lg shadow-lg shadow-gray-600 z-10 w-full lg:w-auto">
-          <div className="flex flex-col lg:flex-row items-center justify-between mb-4">
+          <div className="flex lg:flex-row items-center justify-between mb-4">
             <h6 className="text-xl font-semibold py-4 px-4">Mis datos</h6>
-            <button className="bg-teal-500 font-semibold py-2 px-4 rounded">Editar {`&#9986;`}</button>
+            <button className="bg-teal-500 font-semibold py-2 px-4 rounded flex items-center gap-2">Editar
+              <svg className="w-4 h-4 text-white bg-orange-500 rounded-full" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+              </svg>
+
+            </button>
             <button className="bg-teal-500 font-semibold py-2 px-4 rounded ml-4 h-full" onClick={() => setMisDatos(false)}>X</button>
           </div>
           <div className="flex flex-col items-center justify-between gap-2 text-sm">
