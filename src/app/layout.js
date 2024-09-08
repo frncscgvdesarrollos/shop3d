8a226e4837d6cf3d26f77e9f90471aedaa71057c
frncscgvdesarrollos <frncscgvdesarrollos@gmail.com>
2024-09-08 14:52:43 +0000

    second commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { AuthContextProvider } from '../app/context/AuthContext';
 import './globals.css'
-import Footer from './components/Footer';
 import { Montserrat } from 'next/font/google';
 
 // Configura la fuente
@@ -22,7 +21,6 @@ export default function RootLayout({ children  }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <script src="https://maps.googleapis.com/maps/api/js?key=TU_API_KEY&libraries=places"></script> */}
       </head>
       <body className={`min-h-screen min-w-screen ${montserrat.className}`}>
           <AuthContextProvider>
